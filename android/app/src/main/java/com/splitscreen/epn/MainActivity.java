37d9284f64cfc6b8173f7e4f74443feb31a852c9
TADIO1829 <adrian.cadena@epn.edu.ec>
2025-05-18 10:49:42 -0500

    Splash Screen

---
+++ android/app/src/main/java/com/splitscreen/epn/MainActivity.java
@@ -0,0 +1,17 @@
+package com.splitscreen.epn;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    setTheme(R.style.AppTheme);
+    super.onCreate(savedInstanceState);
+
+  }
+}
+
+
+
